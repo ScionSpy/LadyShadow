5683f599e7ec2297fe31d167165fa43313edd8d7
ScionSpy <29105063+ScionSpy@users.noreply.github.com>
2019-04-06 00:02:56 -0700

    Some new images ~ ERROR

---
+++ Src/System/Bot/Functions/err.js
@@ -0,0 +1,23 @@
+discord = require('discord.js');
+
+module.exports = {
+    coded : "2019-04-05",
+    name : "err",
+    description : "Send Errors to channel in embed format.",
+    usage : "bot.functions.get(`err`).execute(err)",
+
+    execute(message, err){
+        bot = message.client;
+
+        const embed = new discord.RichEmbed()
+          .setTitle("Whoops!! That's an Error!!")
+          .setThumbnail("https://discordapp.com/channels/416906584900239370/485977097358278659/563971234866659339")
+          .setColor("ff0000")
+          .setDescription(`\`\`\`xl\n${err}\`\`\``)
+
+        time = bot.functions.get("date").execute(Date.now());
+        console.log(`\n----------\n\n${time}\n--${message.guild.name} (${message.guild.id}\n--${message.author.tag} (${message.author.id})\n----${message.content}\n------${err}\n`);
+
+        return embed;
+    }
+};
