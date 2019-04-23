74a59ce4b65c9dc64b46b16fc5dc52048a1698af
ScionSpy <29105063+ScionSpy@users.noreply.github.com>
2019-04-22 21:36:44 -0700

    Minor edits

--- Src/System/Bot/Collections/srpg.shop.js
+++ Src/System/Bot/Collections/srpg.shop.js
@@ -2,19 +2,19 @@
 
 module.exports = {
   coded : "2019-03-19",
-  
+
   name : "srpg.shop",
   description : "Logs all of Shadows SRPG Cmds.",
-  
-  
+
+
   execute(bot, fs){
-    
+
 
     const shopFiles = fs.readdirSync('./Src/System/Srpg/Shop').filter(file => file.endsWith('.json'));
 
     for (const file of shopFiles) { 	const item = require(`../../.././System/Srpg/Shop/${file}`);
-      bot.srpg.shop.set(item.name2, item);
+      bot.srpg.shop.set(item.id, item);
     };
-    
+
   }
 };
